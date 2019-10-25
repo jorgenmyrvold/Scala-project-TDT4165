88edf4447232adfeab78411e2b39f5c10fe5d8a2
jorgenmyrvold <jorgeum@stud.ntnu.no>
2019-10-25 14:58:18 +0200

    More added to task 3

--- project_tasks/src/main/scala/Account.scala
+++ project_tasks/src/main/scala/Account.scala
@@ -9,23 +9,28 @@ class Account(val bank: Bank, initialBalance: Double) {
     // TODO
     // for project task 1.2: implement functions
     // for project task 1.3: change return type and update function bodies
-    def withdraw(amount: Double): Either[Double, String] = this.synchronized {
-        if (balance.amount - amount < 0.0 || amount < 0.0) return Right("Funds insufficient")
-        Left(balance.amount - amount)
+    def withdraw(amount: Double): Either[TransactionStatus.Value, TransactionStatus.Value] = this.synchronized {
+        if (balance.amount - amount > 0.0 && amount > 0.0) {
+            balance.amount -= amount
+            return Left(TransactionStatus.SUCCESS)
+        }
+        Right(TransactionStatus.FAILED)
     }
 
-    def deposit (amount: Double): Either[Double,String] = this.synchronized  {
-        if (amount < 0.0) return Right("Can't deposit negative amount")
-        Left(balance.amount + amount)
+    def deposit (amount: Double): Either[TransactionStatus.Value, TransactionStatus.Value] = this.synchronized  {
+        if (amount > 0.0) {
+            balance.amount += amount
+            return Left(TransactionStatus.SUCCESS)
+        }
+        Right(TransactionStatus.FAILED)
     }
-
 
     def getBalanceAmount: Double = this.synchronized{
         balance.amount
     }
 
-    def transferTo(account: Account, amount: Double) = {
-        bank addTransactionToQueue (this, account, amount)
+    def transferTo(account: Account, amount: Double) = this.synchronized {
+        bank addTransactionToQueue(this, account, amount)
     }
 
 }
