4a6846806f66c237ccac1801df19f79b6fd81d34
lisacbr <lisacbr@stud.ntnu.no>
2019-10-28 15:01:49 +0100

    Comments on task 1, 2, and 1.1, 1.2, 1.3

--- project_tasks/src/main/scala/Account.scala
+++ project_tasks/src/main/scala/Account.scala
@@ -9,22 +9,27 @@ class Account(val bank: Bank, initialBalance: Double) {
     // TODO
     // for project task 1.2: implement functions
     // for project task 1.3: change return type and update function bodies
+    
+    //Task 1.2: Account functions
+    //withdraw function: removes an amount of money from the account
     def withdraw(amount: Double): Either[Unit, String] = this.synchronized {
-        if (balance.amount - amount >= 0.0 && amount > 0.0) {
-            balance.amount -= amount
-            return Left()
+        if (balance.amount - amount >= 0.0 && amount > 0.0) {  //checks if withdraw amount is valid
+            balance.amount -= amount 
+            return Left() 
         }
-        Right("Withdraw failed")
+        Right("Withdraw failed") 
     }
 
+    //deposit function: inserts an amount of money to the account
     def deposit (amount: Double): Either[Unit, String] = this.synchronized  {
-        if (amount > 0.0) {
+        if (amount > 0.0) { //checks if deposit amount is a valid number
             balance.amount += amount
             return Left()
         }
         Right("Deposit failed. Tried to deposit negative amount.")
     }
 
+    //getBalanceAmount function: returns the amount of funds in the account.
     def getBalanceAmount: Double = this.synchronized{
         balance.amount
     }
